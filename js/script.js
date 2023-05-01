1546528348960f5a9a9fd6c7d502369cbf279160
CharlyMech <sanchezreciocarlos99@gmail.com>
2023-05-01 19:37:51 +0200

    Hamburger menu animation
    Done with hamburger menu animation, now i'll work on the aside bar to be displayed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 // This script has the same purpose as the styles.css file
 // This script file will provide the basic funtionallity common on every page in this website
 
-document.getElementById('burgerMenu').checked;
 
-function showMenu() {
-	let hamburger = document.getElementById('burgerMenu').checked;
-	let aside = document.getElementsByTagName(aside)
-	if (hamburger) {
-		// aside.style.translate = "0";
-		console.log(hamburger);
-	} else {
-		// aside.style.translate = "-100";
-		console.log(hamburger);
-	}
+// Select the hamburger menu object
+document.querySelector(".hamburger").addEventListener("click", showHamburguerMenu);
+var line1 = document.querySelector(".line1");
+var line2 = document.querySelector(".line2");
+var line3 = document.querySelector(".line3");
+
+function showHamburguerMenu() {
+	line1.classList.toggle("active-line1");
+	line2.classList.toggle("active-line2");
+	line3.classList.toggle("active-line3");
 }
-
-
