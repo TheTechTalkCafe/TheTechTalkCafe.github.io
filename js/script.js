f4acdc605187cbcdd55512fcad38e412816a586a
CharlyMech <sanchezreciocarlos99@gmail.com>
2023-05-14 12:43:16 +0200

    Added JS click outside aside element -> close it

--- js/script.js
+++ js/script.js
@@ -4,14 +4,26 @@
 
 // Select the hamburger menu object
 document.querySelector(".hamburger").addEventListener("click", showHamburguerMenu);
-var line1 = document.querySelector(".line1");
-var line2 = document.querySelector(".line2");
-var line3 = document.querySelector(".line3");
-var aside = document.querySelector(".aside");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
+const aside = document.querySelector(".aside");
+const hamb = document.querySelector(".hamburger");
 
 function showHamburguerMenu() {
 	line1.classList.toggle("active-line1");
 	line2.classList.toggle("active-line2");
 	line3.classList.toggle("active-line3");
 	aside.classList.toggle("aside-active");
+}
+
+// Listener to close ASIDE if user clicks outside when it's active
+
+document.onclick = function (e) {
+	if (!aside.contains(e.target) && !hamb.contains(e.target)) {
+		line1.classList.remove("active-line1");
+		line2.classList.remove("active-line2");
+		line3.classList.remove("active-line3");
+		aside.classList.remove("aside-active");
+	}
 }
