7fb44491308e26acbfd04214d609e5103d677121
CharlyMech <sanchezreciocarlos99@gmail.com>
2023-08-14 20:09:30 +0200

    Update styles and JS main file for the new ASIDE menu
    Still pending to add some animations on the ASIDE using JS, now it very rough and may have some visual bugs

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 // This script has the same purpose as the styles.css file
 // This script file will provide the basic funtionallity common on every page in this website
 
+// DOM for the aside element
+let aside = document.querySelector('#aside');
 
-// Select the hamburger menu object
-document.querySelector(".hamburger").addEventListener("click", showHamburguerMenu);
-const line1 = document.querySelector(".line1");
-const line2 = document.querySelector(".line2");
-const line3 = document.querySelector(".line3");
-const aside = document.querySelector(".aside");
-const hamb = document.querySelector(".hamburger");
-
-function showHamburguerMenu() {
-	line1.classList.toggle("active-line1");
-	line2.classList.toggle("active-line2");
-	line3.classList.toggle("active-line3");
-	aside.classList.toggle("aside-active");
+// Show aside menu function
+function showAside() {
+	aside.style.visibility = "visible";
 }
+
+// Hide aside menu function
+function closeAside() {
+	aside.style.visibility = "hidden";
+}
+
+// TODO: Add listener to document to close aside if user clicks outside the menu
+// TODO: JS Animation on close&open aside
 
 // Listener to close ASIDE if user clicks outside when it's active
 
-document.onclick = function (e) {
-	if (!aside.contains(e.target) && !hamb.contains(e.target)) {
-		line1.classList.remove("active-line1");
-		line2.classList.remove("active-line2");
-		line3.classList.remove("active-line3");
-		aside.classList.remove("aside-active");
-	}
-}
+// document.onclick = function (e) {
+// 	if (!aside.contains(e.target) && !hamb.contains(e.target)) {
+// 		line1.classList.remove("active-line1");
+// 		line2.classList.remove("active-line2");
+// 		line3.classList.remove("active-line3");
+// 		aside.classList.remove("aside-active");
+// 	}
+// }
