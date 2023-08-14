aacf5b5a82fede93b89b6135bb9d81e6c4d53a3e
CharlyMech <sanchezreciocarlos99@gmail.com>
2023-08-14 20:12:27 +0200

    Ups, miss written out instead of our

--- js/welcome.js
+++ js/welcome.js
@@ -18,7 +18,7 @@ var msg = "";
 if (patternES.test(prefLang)) {
 	msg += "Bienvenido a ...\n" + asciiArt + "\nEl podcast de tecnología y más!\n\nSi Estás viendo esto quiere decir que probablemente sepas algo más sobre tecnología... Entonces diviértete con nuestro contenido!"
 } else {
-	msg += "Welcome to ... \n" + asciiArt + "\nThe podcast about tech and more!\n\nIf you are watching this means that you'll probably know something more about tech... So enjoy yourself with out content!!"
+	msg += "Welcome to ... \n" + asciiArt + "\nThe podcast about tech and more!\n\nIf you are watching this means that you'll probably know something more about tech... So enjoy yourself with our content!!"
 }
 
 console.log(msg);
