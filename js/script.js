ac8b8de87b24f08dde4e2e3e8d3ac8d8106c5b8c
CharlyMech <sanchezreciocarlos99@gmail.com>
2023-09-06 21:27:43 +0200

    FEATURE: jokes on footer on load page implemented for both languages

--- js/script.js
+++ js/script.js
@@ -23,3 +23,32 @@ $(document).ready(() => {
 // 		aside.classList.remove("aside-active");
 // 	}
 // }
+
+// Add listener on load page to show a joke in the FOOTER element
+window.addEventListener("load", () => {
+	// jQuery DOM to the JOKE div
+	const jokeDiv = $("#joke");
+	// Constant for the jokes file
+	const jokes = "/files/jokes.json";
+	// Get the File Language Extension to show message
+	var fileLangExtension = location.href.split("/").slice(-1)[0].split('.')[1];
+
+	// Pick a random joke depending on the language extension
+	fetch(jokes)
+		.then(response => {
+			// Checking the response is successful
+			if (!response.ok) {
+				throw new Error("Something went wrong reading the file");
+			}
+			// Parse the response as a JSON
+			return response.json();
+		})
+		.then(data => {
+			jokeDiv.text(data[fileLangExtension][(Math.floor(Math.random() * data[fileLangExtension].length))]);
+		})
+		.catch(error => {
+			// Handling error
+			console.error("Error: ", error);
+
+		})
+})
