4c1ac66af982aa46b8611c079e5a0e0f922c9bbe
CharlyMech <sanchezreciocarlos99@gmail.com>
2023-09-06 19:45:50 +0200

    FIX: fixed changin pages change message lang on console
    FEATURE: now message is read from the files.welcome.json file

--- js/welcome.js
+++ js/welcome.js
@@ -1,28 +1,4 @@
 // This script only shows a message on web browser console
-
-function showConsoleMessage() {
-	// Get the File Language Extension to show message
-	var fileLangExtension = location.href.split("/").slice(-1)[0].split('.')[1];
-
-	let asciiArt = `
-  _______ _       _______        _  _______    _ _     _____       __     
- |__   __| |     |__   __|      | ||__   __|  | | |   / ____|     / _|    
-    | |  | |__   ___| | ___  ___| |__ | | __ _| | | _| |     __ _| |_ ___ 
-    | |  | '_ \\ / _ \\ |/ _ \\/ __| '_ \\| |/ _\` | | |/ / |    / _\` |  _/ _ \\
-    | |  | | | |  __/ |  __/ (__| | | | | (_| | |   <| |___| (_| | ||  __/
-    |_|  |_| |_|\\___|_|\\___|\\___|_| |_|_|\\__,_|_|_|\\_\\\\_____\\__,_|_| \\___|
-`;
-	var msg = "";
-
-	if (fileLangExtension == 'es') {
-		msg += "Bienvenido a ...\n" + asciiArt + "\nEl podcast de tecnología y más!\n\nSi Estás viendo esto quiere decir que probablemente sepas algo más sobre tecnología... Entonces diviértete con nuestro contenido!"
-	} else {
-		msg += "Welcome to ... \n" + asciiArt + "\nThe podcast about tech and more!\n\nIf you are watching this means that you'll probably know something more about tech... So enjoy yourself with our content!!"
-	}
-
-	console.log(msg);
-}
-
 function showJSONMessage() {
 	// ASCII Art constant variable
 	let asciiArt = `
@@ -68,8 +44,5 @@ function showJSONMessage() {
 }
 
 window.addEventListener("load", () => {
-	showConsoleMessage();
-	showJSONMessage(); // TESTING -> // TODO: solve asciiArt displaying bad
+	showJSONMessage();
 })
-
-// TODO -> On changing languages, display correct message (now it persists)
